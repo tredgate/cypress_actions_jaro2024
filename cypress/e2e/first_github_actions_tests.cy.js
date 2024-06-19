9d5c8f52e494031bc435644614267ce650255a1b
Petr Fifka <petrfifk@gmail.com>
2024-06-19 20:08:31 +0200

    add new test

--- cypress/e2e/first_github_actions_tests.cy.js
+++ cypress/e2e/first_github_actions_tests.cy.js
@@ -4,4 +4,9 @@ describe("Github Actions Tests", () => {
     cy.get("#username").type("admin");
     cy.get("#username").should("have.value", "admin");
   });
+
+  it("Login Header exist", () => {
+    cy.visit("https://tredgate.com/pmtool");
+    cy.get("h3.form-title").should("exist");
+  });
 });
