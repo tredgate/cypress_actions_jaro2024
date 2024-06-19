f4aded2f69bcc566257c050b4e3391d31544ca66
Petr Fifka <petrfifk@gmail.com>
2024-06-19 19:34:23 +0200

    init github actions

---
+++ cypress/e2e/first_github_actions_tests.cy.js
@@ -0,0 +1,7 @@
+describe("Github Actions Tests", () => {
+  it("Open pmtool, fill username and check its value", () => {
+    cy.visit("https://tredgate.com/pmtool");
+    cy.get("#username").type("admin");
+    cy.get("#username").should("have.value", "admin");
+  });
+});
